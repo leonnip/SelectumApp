928d0d9ddeb3f59bee7516c1e48ddedc3b74690c
Leonni Perez <leonnip@maxkode.com>
2026-03-23 00:44:41 +0100

    Actualizando

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+// Ruta básica
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando 🚀');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
